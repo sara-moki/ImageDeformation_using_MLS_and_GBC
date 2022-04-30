7dc6488cec97886336f0d1086d102c45c9d0b377
ساره <99841907+sara-moki@users.noreply.github.com>
2022-04-30 19:31:37 +0300

    Delete Region-basedImage_Deformation directory

--- Region-basedImage_Deformation/findClosestControl.m
+++
@@ -1,7 +0,0 @@
-function [indexFound]=findClosestControl(hnewPoint,pointsXY)
-nPoints=size(pointsXY,1);
-normpoints = zeros(1,nPoints);
-for i=1:nPoints
-    normpoints(i) = norm(pointsXY(i,:)-hnewPoint);
-end
-[C,indexFound] = min(normpoints);
